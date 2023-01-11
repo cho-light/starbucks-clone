b9fcd6ea48dfe82b24d4b3716964764b7544f9af
unknown <jki0322@gmail.com>
2023-01-11 19:36:02 +0900

    banner
    bean, ethiopia 작업완료
    pick your favorite 작업중

--- js/main.js
+++ js/main.js
@@ -78,3 +78,36 @@ promotionToggleBtn.addEventListener("click", function () {
     promotionEl.classList.remove("show");
   }
 });
+
+
+
+const beanimgfadeEl= document.querySelector('.main-bean .bean-img-box')
+const beantxtfadeEl= document.querySelector('.main-bean .bean-txt-box')
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 550) {
+        gsap.to(beanimgfadeEl, 1, {
+          opacity: 1,
+          left:150
+        });
+        gsap.to(beantxtfadeEl, 1, {
+          opacity: 1,
+          right:150
+        });
+      
+    }
+    if(window.scrollY < 450){
+        gsap.to(beanimgfadeEl, 1, {
+          opacity: 0,
+          left:-600
+      });
+      gsap.to(beantxtfadeEl, 1, {
+        opacity: 0,
+        right:-600
+    });
+    }
+      
+    
+  })
+);
